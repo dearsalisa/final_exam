9b48064b13c7a4b42dc7e8610dc48e7ba535e131
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-08-04 17:20:33 +0700

    add more testcase

--- exam02/src/test/java/Exam02Test.java
+++ exam02/src/test/java/Exam02Test.java
@@ -20,4 +20,34 @@ public class Exam02Test {
         assertEquals(190, actualResult);
     }
 
+    @Test
+    public void buy_1A_1B_1C_should_price_190() {
+        exam02.addOrder('A');
+        exam02.addOrder('B');
+        exam02.addOrder('C');
+        int actualResult = exam02.calculateMoney();
+        assertEquals(270, actualResult);
+    }
+
+    @Test
+    public void buy_1A_1B_1C_1D_should_price_190() {
+        exam02.addOrder('A');
+        exam02.addOrder('B');
+        exam02.addOrder('C');
+        exam02.addOrder('D');
+        int actualResult = exam02.calculateMoney();
+        assertEquals(320, actualResult);
+    }
+
+    @Test
+    public void buy_1A_1B_1C_1D_1E_should_price_190() {
+        exam02.addOrder('A');
+        exam02.addOrder('B');
+        exam02.addOrder('C');
+        exam02.addOrder('D');
+        exam02.addOrder('E');
+        int actualResult = exam02.calculateMoney();
+        assertEquals(425, actualResult);
+    }
+
 }
