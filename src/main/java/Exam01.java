c22dad54c244b7459a9067d29286f17ed1a5890a
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-08-04 16:06:12 +0700

    add function findMaxNumber and function check add new testcase

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -1,6 +1,36 @@
 public class Exam01 {
-    public int max_number(int input) {
-        if(input == 10) return input-1;
-        return input;
+    public long max_number(long input) {
+        if(input <= 9) return input;
+        else {
+            String numberStr = Long.toString(input);
+            String firstTwoDigits = findMaxNumber(input);
+            for (int i = 0; i <numberStr.length()-2 ; i++) {
+                firstTwoDigits+="9";
+            }
+            return Long.parseLong(firstTwoDigits);
+        }
+    }
+
+    public String findMaxNumber(long number) {
+        int firstDigit = Integer.parseInt(Long.toString(number).substring(0, 1));
+        int secondDigit = Integer.parseInt(Long.toString(number).substring(1, 2));
+        int result = (firstDigit*10)+secondDigit;
+
+        while (result > 9) {
+            result--;
+            if(result <= 9) return Integer.toString(result);
+            if(check(result)) return Integer.toString(result);
+        }
+        System.out.println("eiei = "+result);
+        return Integer.toString(result);
+    }
+
+    public boolean check(int number) {
+        boolean result = true;
+        String numberStr = Integer.toString(number);
+        for (int i = 0; i < numberStr.length()-1 ; i++) {
+            if(Character.getNumericValue(numberStr.charAt(i)) > Character.getNumericValue(numberStr.charAt(i+1))) result=false;
+        }
+        return result;
     }
 }
--- src/test/java/Exam01Test.java
+++ src/test/java/Exam01Test.java
@@ -30,4 +30,38 @@ public class Exam01Test {
         long actualResult = exam01.max_number(132);
         assertEquals(129, actualResult);
     }
+
+    @Test
+    public void should_say_99999999999999999_when_number_is_111111111111111110() {
+        long actualResult = exam01.max_number(111111111111111110L);
+        assertEquals(99999999999999999L, actualResult);
+    }
+
+    public void should_say_989999999999999999_when_number_is_899999999999999999() {
+        long actualResult = exam01.max_number(899999999999999999L);
+        assertEquals(989999999999999999L, actualResult);
+    }
+
+    public void should_say_599999999999999999_when_number_is_653161544328834058() {
+        long actualResult = exam01.max_number(653161544328834058L);
+        assertEquals(599999999999999999L, actualResult);
+    }
+
+    @Test
+    public void should_say_0_when_number_is_0() {
+        long actualResult = exam01.max_number(0);
+        assertEquals(0, actualResult);
+    }
+
+    @Test
+    public void should_say_1_when_number_is_1() {
+        long actualResult = exam01.max_number(8);
+        assertEquals(8, actualResult);
+    }
+
+    @Test
+    public void should_say_45_when_number_is_45() {
+        long actualResult = exam01.max_number(45);
+        assertEquals(45, actualResult);
+    }
 }
